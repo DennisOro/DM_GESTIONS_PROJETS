871e48163965917a54c7c213cd5c5431f8bc2b99
Lado Khelya <lado.k252@gmail.com>
2019-05-12 20:45:15 -0400

    Login Implementation. Il y a encore de bogues à coriger.

--- GestionProjet/GestionProjet/GestionProjet/Controllers/HomeController.cs
+++ GestionProjet/GestionProjet/GestionProjet/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,9 +14,43 @@ namespace GestionProjet.Controllers
     {
         public ActionResult Index()
         {
+            dynamic dynamicModel = new ExpandoObject();
+
             List<Project> projectsList = getProjectsFromDatabase();
 
-            return View(projectsList);
+            dynamicModel.Login = new Login();
+
+            dynamicModel.projectsList = getProjectsFromDatabase();
+
+            dynamicModel.Login.Message = "ssssss";
+
+            return View(dynamicModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(dynamic dynamicModel)
+        {
+            string userID = Request["userIDText"];
+            string password = Request["password"];
+
+            string message = "";
+
+            if (testLogin(userID, password))
+            {
+                //dynamicModel.Login.Message = "Authentification valide";
+                message = "Authentification valide";
+            }
+            else
+            {
+                message = "Authentification invalide";
+            }
+
+            Login login = new Login();
+            login.Message = message;
+
+            ViewData["MyLogin"] = login;
+
+            return View(dynamicModel);
         }
 
         public ActionResult About()
@@ -34,7 +69,24 @@ namespace GestionProjet.Controllers
 
         public ActionResult Login()
         {
-            return View("Login");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login loginModel)
+        {
+            
+            if (testLogin(loginModel.UserID, loginModel.Password))
+            {
+                loginModel.Message = "Authentification valide";
+            }
+            else
+            {
+                loginModel.Message = "Authentification invalide";
+            }
+            
+
+            return View(loginModel);
         }
 
         private List<Project> getProjectsFromDatabase()
@@ -62,8 +114,7 @@ namespace GestionProjet.Controllers
                     }
                     finally
                     {
-                        // Always call Close when done reading.
-                        reader.Close();
+                          reader.Close();
                     }
 
                     conn.Close();
@@ -77,6 +128,53 @@ namespace GestionProjet.Controllers
 
             return projectsList;
 
+        }
+
+        private bool testLogin(string userID, string password)
+        {
+            userID = userID == null ? "" : userID.Trim();
+            password = password == null ? "" : password.Trim();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = SqlDatabaseConnection.CONNECTIONSTRING;
+
+                    conn.Open();
+
+                    string query = @"select * from Login";
+
+                    SqlCommand command = new SqlCommand(query, conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            string storedUserID = reader[0] == null ? "" : reader[0].ToString().Trim();
+                            string storedPassw = reader[1] == null ? "" : reader[1].ToString().Trim();
+
+                            if (storedUserID == userID && storedPassw == password)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        reader.Close();
+                    }
+
+                    conn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
         }
     }
 }
--- GestionProjet/GestionProjet/GestionProjet/Models/Login.cs
+++ GestionProjet/GestionProjet/GestionProjet/Models/Login.cs
@@ -6,14 +6,16 @@ using System.Web;
 
 namespace GestionProjet.Models
 {
-    public class LoginModel
+    public class Login
     {
         [Required(ErrorMessage = "Nom d'utilisateur est obligatoire")]
-        public string UserName { get; set; }
+        public string UserID { get; set; }
 
         [Required(ErrorMessage = "Mot de passe est obligatoire")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        public string Message { get; set; }
 
     }
 }
