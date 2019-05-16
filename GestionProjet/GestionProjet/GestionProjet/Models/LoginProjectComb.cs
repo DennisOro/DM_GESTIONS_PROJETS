0c7c4a3b19cdd6d8e05948604bd584d71cd4dbd8
Lado Khelya <lado.k252@gmail.com>
2019-05-15 22:30:34 -0400

    Validation de Login fonctionne. Login est Projets sont maintenant affichés sur la meme page.

---
+++ GestionProjet/GestionProjet/GestionProjet/Models/LoginProjectComb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionProjet.Models
+{
+    public class LoginProjectComb
+    {
+        public Login Login { get; set; }
+        public List<Project> projectsList { get; set; }
+    }
+}
