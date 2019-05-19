0ee0a18752900264cd2cb2e880e78b1c1a70e832
Filip Kulach <kulach10_@hotmail.com>
2019-05-19 01:18:34 -0400

    Creation vue et modele admin

---
+++ GestionProjet/GestionProjet/GestionProjet/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using GestionProjet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionProjet.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        // GET: Admin
+        [HttpGet]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Admin(FormCollection formCollection)
+        {
+            Admin employe = new Admin();
+            employe.matricule = Convert.ToInt32(formCollection["Matricule"]);
+            employe.nom = formCollection["Nom"];
+            employe.prenom = formCollection["Prenom"];
+            employe.login = formCollection["Login"];
+            employe.motDePasse  = formCollection["MotDePasse"];
+            AjouterEmploye(employe);
+            return View();
+        }
+
+        public void AjouterEmploye(Admin employe)
+        {
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = SqlDatabaseConnection.CONNECTIONSTRING;
+
+
+
+                SqlCommand cmd = new SqlCommand("spCreerEmploye", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramNom = new SqlParameter();
+                paramNom.ParameterName = "@nom";
+                paramNom.Value = employe.nom;
+                cmd.Parameters.Add(paramNom);
+
+                SqlParameter paramPrenom = new SqlParameter();
+                paramPrenom.ParameterName = "@prenom";
+                paramPrenom.Value = employe.prenom;
+                cmd.Parameters.Add(paramPrenom);
+
+                SqlParameter paramLogin = new SqlParameter();
+                paramLogin.ParameterName = "@login";
+                paramLogin.Value = employe.login;
+                cmd.Parameters.Add(paramLogin);
+
+                SqlParameter parammotDePasse = new SqlParameter();
+                parammotDePasse.ParameterName = "@motDePasse";
+                parammotDePasse.Value = employe.motDePasse;
+                cmd.Parameters.Add(parammotDePasse);
+
+        
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ GestionProjet/GestionProjet/GestionProjet/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionProjet.Models
+{
+    public class Admin
+    {
+        public int matricule { get; set; }
+        public String nom { get; set; }
+        public String prenom { get; set; }
+        public String login { get; set; }
+        public String motDePasse { get; set; }
+
+
+    }
+}
