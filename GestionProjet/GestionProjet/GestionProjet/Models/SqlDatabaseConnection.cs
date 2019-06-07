9e502dcd32cffb94ccb449a2696b1e3eb8c4d64d
Lado Khelya <lado.k252@gmail.com>
2019-06-07 19:31:39 -0400

    Chargement android studio, stop tracking.

--- GestionProjet/GestionProjet/GestionProjet/Models/SqlDatabaseConnection.cs
+++ GestionProjet/GestionProjet/GestionProjet/Models/SqlDatabaseConnection.cs
@@ -9,6 +9,8 @@ namespace GestionProjet.Models
     {
         //public static string CONNECTIONSTRING = @"Data Source=LADO\SQLEXPRESS; Initial Catalog=INF6150; Integrated Security=SSPI;";
         //public static string CONNECTIONSTRING = @"Data Source=DESKTOP-275FO6M\SQLEXPRESS; Initial Catalog=INF6150; Integrated Security=SSPI;";
-        public static string CONNECTIONSTRING = @"Data Source=LAPTOP-EBF0KJE9\SQLEXPRESS; Initial Catalog=INF6150; Integrated Security=SSPI;";
+        //public static string CONNECTIONSTRING = @"Data Source=172.29.111.49,1433; Initial Catalog=INF6150; Network Library=DBMSSOCN; User ID=Lado_login; Password=pass";
+
+
     }
 }
